d6494ce7b77e7f4aa538457f2d80a949aba47184
joodrew <41982701+joodrew@users.noreply.github.com>
2018-11-14 00:35:43 -0200

    caroulsel

---
+++ carousel.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function () {
+  $('.certificado-carousel').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      responsive: [
+          {
+              breakpoint: 768,
+              settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 3
+              }
+          },
+          {
+              breakpoint: 480,
+              settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 1
+              }
+          }
+      ]
+  });
+});
