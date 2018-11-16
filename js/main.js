da73476290944301d566d72b37df94ff55d8d31a
joodrew <41982701+joodrew@users.noreply.github.com>
2018-11-15 22:07:16 -0200

    aa

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// navbar uncollapse
-$(function() {
-    $('.nav a').on('click', function(){ 
-        if($('.navbar-toggler').css('display') !='none'){
-            $('.navbar-toggler').trigger( "click" );
-        }
-    });
-});
