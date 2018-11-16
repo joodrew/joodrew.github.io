4192199aced37de66e37c3ebbc9f27831758ce74
joodrew <41982701+joodrew@users.noreply.github.com>
2018-11-16 05:28:38 -0200

    n

--- js/carousel.js
+++ js/carousel.js
@@ -38,9 +38,15 @@ $(document).ready(function () {
         }
       },
       {
-        breakpoint: 520,
+        breakpoint: 420,
         settings: {
           slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1
         }
       }
     ]
@@ -50,8 +56,39 @@ $(document).ready(function () {
     infinite: true,
     speed: 300,
     dots: false,
-    arrows:false,
+    arrows: false,
     autoplay: true,
-    slidesToShow: 1
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1 
+        }
+        },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 });
